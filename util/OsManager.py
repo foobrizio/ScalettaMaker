be12e73b1c12dccebc409d44819c120ca1246ec2
Fabrizio Gabriele <fabriziogabriele@proton.me>
2023-08-01 21:30:42 +0200

    Linux folder specification

--- util/OsManager.py
+++ util/OsManager.py
@@ -19,17 +19,26 @@ class OsManager:
     def get_song_dir():
         if os.name == 'nt':
             return Consts.__WIN_SONG_DIR__
-        return Consts.__LX_SONG_DIR__
+        elif os.name == 'posix':
+            return Consts.__LX_SONG_DIR__
+        else:
+            raise Exception(os.name+' OS is not currently supported')
 
     @staticmethod
     def get_result_dir():
         if os.name == 'nt':
             return Consts.__WIN_RESULT_DIR__
-        return Consts.__LX_RESULT_DIR__
+        elif os.name == 'posix':
+            return Consts.__LX_RESULT_DIR__
+        else:
+            raise Exception(os.name + ' OS is not currently supported')
 
     @staticmethod
     def get_data_dir():
         if os.name == 'nt':
             return Consts.__WIN_DATA_DIR__
-        return Consts.__LX_DATA_DIR__
+        elif os.name == 'posix':
+            return Consts.__LX_DATA_DIR__
+        else:
+            raise Exception(os.name+' OS is not currently supported')
 
